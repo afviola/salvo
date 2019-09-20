6e2547585058fd80198ca5e086087a85d23db521
mmm <>
2019-09-20 12:24:39 -0300

    agregado front de prueba para el leaderboard

--- src/main/resources/static/web/games.js
+++ src/main/resources/static/web/games.js
@@ -1,8 +1,12 @@
 
 $('document').ready(function() {
     $.get('/api/games')
-        .done((games) => { createGameList(games); })
-        .fail((jqHRX, status) => { console.error(status); });
+        .done((games) => { createGameList(games) })
+        .fail((jqHRX, status) => { console.error(status) });
+
+    $.get('/api/leaderboard')
+        .done((players) => { createLeaderboard(players) })
+        .fail((jqHRX, status) => { console.error(status) });
 });
 
 function createGameList(games) {
@@ -15,6 +19,23 @@ function createGameList(games) {
                 ${game.gamePlayers[0].player.email} ;
                 ${game.gamePlayers[1].player.email}
             </li>`
+        );
+    });
+}
+
+function createLeaderboard(players) {
+    let leaderboard = $('#leaderboard');
+
+    players.forEach(player => {
+        leaderboard.append(
+            `<li>
+                ${ player.name }
+                ${ player.total }
+                ${ player.won }
+                ${ player.lost }
+                ${ player.tied }
+             </li>
+            `
         );
     });
 }
