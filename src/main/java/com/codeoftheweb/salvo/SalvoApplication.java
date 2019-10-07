1319423c6f71fe2c0422db518022236333944d5a
afviola <aibbyale@gmail.com>
2019-10-07 12:03:12 -0300

    mejoras en la autorizacion

--- src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
+++ src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
@@ -104,7 +104,13 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-				.antMatchers("/api/**").hasAuthority("USER")
+				.antMatchers(
+						"/web/**",
+						"/api/logout",
+						"/api/leaderboard",
+						"/api/game_view/*",
+						"/api/games").permitAll()
+				.anyRequest().authenticated()
 				.and()
 				.formLogin()
 				.usernameParameter("username")
